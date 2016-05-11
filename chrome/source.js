d0e09ca4aa1ec27df2b5f8bce2902ac9677f80f5
gewure <jasperreichardt@gmail.com>
2016-05-11 16:24:08 +0200

    -> bierfreunde Feature :)

--- chrome/source.js
+++ chrome/source.js
@@ -1,56 +1,60 @@
-window.setTimeout(function(){walk(document.body);}, 100);
+//
+//
+//
+//
+window.setTimeout(function () {
+    walk(document.body);
+}, 300);
 //window.setInterval(function(){walk(document.body);}, 30000);
 
 walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
+// walk all nodes
+function walk(node) {
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+    var child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    switch (node.nodeType) {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment -> move into this.
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node - substitute it :)
+            handleText(node);
+            break;
+    }
 }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+function handleText(textNode) {
+    var v = textNode.nodeValue;
 
-        v = v.replace(/\bApple\b/g, "Banana");
-        v = v.replace(/\bapple\b/g, "banana");
-        v = v.replace(/\bApples\b/g, "Bananas");
-        v = v.replace(/\bapples\b/g, "bananas");
-        v = v.replace(/\bApple's\b/g, "Banana's");
-        v = v.replace(/\bapple's\b/g, "banana's");
-        v = v.replace(/\bAPPLE\b/g, "BANANA");
-        v = v.replace(/\biWatch\b/g, "bananaWatch");
-        v = v.replace(/\biwatch\b/g, "bananaWatch");
-        v = v.replace(/\bTim Cook\b/g, "Pirate Hook");
-        v = v.replace(/\biPhone\b/g, "bananaPhone");
-        v = v.replace(/\biphone\b/g, "bananaPhone");
-        v = v.replace(/\bipad\b/g, "bananaPad");
-        v = v.replace(/\biPad\b/g, "bananaPad");
-        v = v.replace(/\bSteve Jobs\b/g, "Albus Dumbledore");
-        v = v.replace(/\biOS\b/g, "bananaOS");
-	v = v.replace(/\bMacBook\b/g, "bananaBook");
-	v = v.replace(/\bAppleTV\b/g, "Tell-Lie-Vision");
-	textNode.nodeValue = v;
+    v = v.replace(/\bFPÖ\b/g, "FBÖ");
+    v = v.replace(/\bFreiheitliche\bPartei\bÖsterreichs\b/g, "Freunde der Biere Österreichs");
+    v = v.replace(/\bH\.C.\bStrache\b/g, "K.C. Streichel");
+    v = v.replace(/\bHerbert\bKickl\b/g, "Propagandaminister Görckl");
+    v = v.replace(/\bAsylwerber\b/g, "Menschen");
+    v = v.replace(/\bFlüchtlinge\b/g, "Menschen");
+    v = v.replace(/\bFlüchtling\b/g, "Menschen");
+    v = v.replace(/\bNorber\bHofer\b/g, "Andreas Hofer");
+    v = v.replace(/\bTS\b/g, "Stronach'schen Erfüllungsgehilfen");
+    v = v.replace(/\bVan\bder\Bellen\b/g, "Vreund der Berge");
+    v = v.replace(/\bHeinz\bFischer\b/g, "Richard Lugner");
+    v = v.replace(/\Hofburg\b/g, "Lugnercity");
+    v = v.replace(/\bSPÖ\b/g, "sPÖ");
+    v = v.replace(/\Harald\bVilimsky\b/g, "Jimmy Kaltschnautze");
+    v = v.replace(/\bÖVP\b/g, "Betonierer&Co GmbH");
+    v = v.replace(/\bIdentitären\b/g, "Reserve-SA");
+    v = v.replace(/\bBurschenschaft\b/g, "Paukpommeranzen");
+    v = v.replace(/\bdie\bWähler\b/g, "die Schafe");
+    v = v.replace(/\bBundespräsident\b/g, "Oberster des Rates der Weisen");
+    textNode.nodeValue = v;
 }
