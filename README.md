8584d1aad3bfcfba304e67b5fce41d408e0f1523
gewure <jasperreichardt@gmail.com>
2016-05-11 19:17:51 +0200

    chromebuild v1.2

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # iBracer
 
-substitutes some text in the webpages you browse, in particular: Apple related words ;) 
-works fast and reliable to free you from annoying Apple products, articles and propaganda.
+substitutes some text in the webpages you browse, in particular: FPÖ related words.
+This is meant as political Satire and doesn't want to offend anyone else than the ones who are seriously offended.
 
 v2.0 with on/off button and fancy CSS comming soon.
-
 
 =========
 
@@ -17,7 +16,7 @@ alternative: goto chrome://extensions , chose "load extension" pick chrome.crx
 Mozilla Firefox:
 --------------
 download pirate_hook_banana.xpi
-choose Options/Add-Ons go to 'install Add-On from File' -> choose pirate_hook_banana.xpi
+choose Options/Add-Ons go to 'install Add-On from File' -> choose austria_app.xpi
 
 =========
 You like this?
@@ -26,4 +25,4 @@ Consider donating Bitcoin to: 19rxGgooYW65Hdja47ysxgpaJcke2jroHj
 
 Credits go to the original coders at https://github.com/panicsteve/cloud-to-butt/
 
-aaad & gewure, 2015
+gewure & friends, 2016
--- chrome/source.js
+++ chrome/source.js
@@ -1,14 +1,14 @@
 //
 //
+// Author: Gewure & aaad, 2016
 //
-//
-window.setTimeout(function () {
-    walk(document.body);
-}, 300);
+//window.setTimeout(function () {
+//    walk(document.body);
+//}, 1000);
 //window.setInterval(function(){walk(document.body);}, 30000);
 
 walk(document.body);
-
+//document.onload(walk(document.body));
 // walk all nodes
 function walk(node) {
     // I stole this function from here:
@@ -31,30 +31,42 @@ function walk(node) {
         case 3: // Text node - substitute it :)
             handleText(node);
             break;
+        default:
     }
 }
 
 function handleText(textNode) {
     var v = textNode.nodeValue;
 
+    v = v.replace(/\bStrache\b/g, "Streichel");
     v = v.replace(/\bFPÖ\b/g, "FBÖ");
-    v = v.replace(/\bFreiheitliche\bPartei\bÖsterreichs\b/g, "Freunde der Biere Österreichs");
-    v = v.replace(/\bH\.C.\bStrache\b/g, "K.C. Streichel");
-    v = v.replace(/\bHerbert\bKickl\b/g, "Propagandaminister Görckl");
-    v = v.replace(/\bAsylwerber\b/g, "Menschen");
-    v = v.replace(/\bFlüchtlinge\b/g, "Menschen");
-    v = v.replace(/\bFlüchtling\b/g, "Menschen");
-    v = v.replace(/\bNorber\bHofer\b/g, "Andreas Hofer");
+    v = v.replace(/FPÖ/g, "FBÖ");
+    v = v.replace(/ÖVP/g, "Betonierer&Co GmbH");
+    v = v.replace(/\bFreiheitliche Partei Österreichs\b/g, "Freunde der Biere Österreichs");
+    v = v.replace(/\bH\.C\. Strache\b/g, "K.C. Streichel");
+    v = v.replace(/Heinz-Christian Strache/g, "K.C. Streichel");
+    v = v.replace(/Herbert Kickl/g, "Propagandaminister Görckl");
+    v = v.replace(/Asylwerber/g, "Menschen");
+    v = v.replace(/Flüchtlinge/g, "Menschen");
+    v = v.replace(/Flüchtling/g, "Menschen");
+    v = v.replace(/Flüchtlingen/g, "Menschen");
+    v = v.replace(/Norbert Hofer/g, "Andreas Hofer");
     v = v.replace(/\bTS\b/g, "Stronach'schen Erfüllungsgehilfen");
-    v = v.replace(/\bVan\bder\Bellen\b/g, "Vreund der Berge");
-    v = v.replace(/\bHeinz\bFischer\b/g, "Richard Lugner");
-    v = v.replace(/\Hofburg\b/g, "Lugnercity");
+    v = v.replace(/Van der Bellen/g, "Vreund der Berge");
+    v = v.replace(/\bHeinz Fischer\b/g, "Richard Lugner");
+    v = v.replace(/\bHofburg\b/g, "Lugnercity");
     v = v.replace(/\bSPÖ\b/g, "sPÖ");
-    v = v.replace(/\Harald\bVilimsky\b/g, "Jimmy Kaltschnautze");
+    v = v.replace(/\bHarald Vilimsky\b/g, "Jimmy Kaltschnautze");
     v = v.replace(/\bÖVP\b/g, "Betonierer&Co GmbH");
     v = v.replace(/\bIdentitären\b/g, "Reserve-SA");
-    v = v.replace(/\bBurschenschaft\b/g, "Paukpommeranzen");
-    v = v.replace(/\bdie\bWähler\b/g, "die Schafe");
+    v = v.replace(/Burschenschaft/g, "Paukpommeranzen");
+    v = v.replace(/\bdie Wähler\b/g, "die Schafe");
     v = v.replace(/\bBundespräsident\b/g, "Oberster des Rates der Weisen");
+    v = v.replace(/blau/g, "hackeblau");
+    v = v.replace(/Parteifreunde/g, "Saufkumpanen");
+
+
+    //TODo :)
     textNode.nodeValue = v;
+
 }
